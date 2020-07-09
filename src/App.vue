90614237468e3f743dd76e0318a1af32739e846f
Janet Liang <janet@harmony.one>
2020-07-08 20:04:53 -0700

    [app] Update frontend to work with faucet backend design

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             <span class="name nunito">Harmony Testnet BUSD Faucet</span>
           </v-row>
           <v-row align="center" justify="center">
-            <span class="nunito">Enter your ONE address to get {{ busdAmount }} BUSD</span>
+            <span class="nunito">Enter your ONE address to get {{ amount }} BUSD</span>
           </v-row>
           <v-row align="center" justify="center">
             <v-card class="mt-3 pr-2"
@@ -25,6 +25,23 @@
               </v-text-field>
             </v-card>
           </v-row>
+          <v-row v-if="loading" align="center" justify="center">
+            <v-progress-circular class="mt-4"
+                                 color="light-blue"
+                                 indeterminate>
+            </v-progress-circular>
+          </v-row>
+          <v-row v-if="successful && hash != null" align="center" justify="center" class="mt-2">
+              <span class="nunito">Transaction successful!</span>
+          </v-row>
+          <v-row v-if="successful && hash != null" align="center" justify="center" class="mt-1">
+            <a v-bind:href="link" target="_blank">
+              <span class="nunito">{{ hash }}</span>
+            </a>
+          </v-row>
+          <v-row v-if="!successful" align="center" justify="center" class="mt-2">
+            <span class="nunito">Error sending transaction. Please try again.</span>
+          </v-row>
         </v-container>
       </v-container>
     </v-main>
@@ -32,31 +49,43 @@
 </template>
 
 <script>
-  const { Harmony } = require('@harmony-js/core')
-  const { ChainType } = require('@harmony-js/utils')
-
-  let endpoint = 'https://api.s0.b.hmny.io'
-  let chainid = 2
-
-  const hmy = new Harmony(endpoint,
-          {
-            chainType: ChainType.Harmony,
-            chainID: chainid,
-          }
-  )
-  
   export default {
     name: 'App',
     data: () => ({
-      busdAmount: 1, // How much is a good amount to send out, change when done testing
+      amount: 1, // How much is a good amount to send out, change when done testing
       toAddress: "",
+      loading: false,
+      successful: true,
+      hash: null,
+      link: null,
     }),
     methods: {
-      sendTransaction() {
-        console.log("sending transaction for " + this.busdAmount + " to " + this.toAddress + " from ")
-      },
-      oneToHexAddress(hmy, address) {
-        return hmy.crypto.getAddress(address).basicHex
+      async sendTransaction() {
+        console.log("sending transaction for " + this.amount + " to " + this.toAddress)
+        let requestString = 'http://localhost:3000/tx?method=transfer&args=' + this.toAddress + ',' + (this.amount * 1e18)
+        console.log(requestString)
+        this.loading = true
+        this.hash = null
+        this.link = null
+
+        fetch(requestString, { mode: 'cors', headers: {
+            'Content-Type': 'application/json',
+          }})
+                .then(response => {
+                  this.loading = false
+                  if (!response.ok) {
+                    this.successful = false
+                    throw new Error('Network response was not ok');
+                  }
+                  this.successful = true
+                  response.json().then(resp => {
+                    this.hash = resp.hash
+                    this.link = 'https://explorer.testnet.harmony.one/#/tx/' + resp.hash
+                  })
+                })
+                .catch(error => {
+                  console.error('Error:', error);
+                })
       },
     }
   };
